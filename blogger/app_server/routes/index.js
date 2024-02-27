1a045f4fd459f0a322166178935a0cd6f6da8db0
kuhaku4 <156698364+kuhaku4@users.noreply.github.com>
2024-02-26 21:26:26 -0500

    Update index.js

--- blogger/app_server/routes/index.js
+++ blogger/app_server/routes/index.js
@@ -5,8 +5,15 @@ var crtlBlog = require('../controllers/blog');
 
 /* GET home page. */
 router.get('/', crtlHome.home);
+router.post('/complete/:id', crtlBlog.commitComplete);
 router.get('/list', crtlBlog.list);
-router.get('/add', crtlBlog.add);
-router.get('/')
+router.get('/add', crtlBlog.addBlog);
+router.post('/add', crtlBlog.saveBlog);
+router.get('/delete/:id',crtlBlog.deleteTask);
+router.post('/delete/:id',crtlBlog.confirmDelete);
+router.get('/edit/:id',crtlBlog.blogEdit);
+router.post('/edit/:id',crtlBlog.commitEdit);
+
+router.get('/add')
 /* TEST7*/
 module.exports = router;
