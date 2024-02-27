f29ebaebcb16efb58b484d45b8946bf5c5021f39
kuhaku4 <edalmonr@millersville.edu>
2024-02-27 00:09:45 +0000

    iubawd

---
+++ .vscode-server/data/User/History/-7653b282/wP1D.js
@@ -0,0 +1,116 @@
+var mongoose = require('mongoose');
+var Blog = mongoose.model('Blog');
+var request = require('request');
+
+var sendJSONresponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+/* GET a blog by the id */
+module.exports.blogsReadOne = function(req, res) {
+  console.log('Finding blog details', req.params);
+  if (req.params && req.params.blogid) {
+    Blog
+      .findById(req.params.blogid)
+      .exec(function(err, blog) {
+        if (!blog) {
+          sendJSONresponse(res, 404, {
+            "message": "blogid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(blog);
+        sendJSONresponse(res, 200, blog);
+      });
+  } else {
+    console.log('No blogid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No blogid in request"
+    });
+  }
+};
+
+/* POST a new blog */
+/* /api/blogs */
+module.exports.blogsCreate = function(req, res) {
+  console.log(req.body);
+  Blog.create({
+    blogtitle: req.body.blogtitle,
+    blogtext: req.body.blogtext,
+    createdeDate: req.body.createdDate
+  }, function(err, blog) {
+    if (err) {
+      console.log(err);
+      sendJSONresponse(res, 400, err);
+    } else {
+      console.log(blog);
+      sendJSONresponse(res, 201, blog);
+    }
+  });
+};
+
+
+/* PUT /api/blog/:blogid */
+module.exports.blogsUpdateOne = function(req, res) {
+  if (!req.params.blogid) {
+    sendJSONresponse(res, 404, {
+      "message": "Not found, blogid is required"
+    });
+    return;
+  }
+  Blog
+    .findById(req.params.blogid)
+    .select('-blogtitle -blogtext')
+    .exec(
+      function(err, blog) {
+        if (!blog) {
+          sendJSONresponse(res, 404, {
+            "message": "blogid not found"
+          });
+          return;
+        } else if (err) {
+          sendJSONresponse(res, 400, err);
+          return;
+        }
+        blog.blogtitle = req.body.blogtitle;
+        blog.blogtext = req.body.blogtext;
+        blog.createdDate = req.body.createdDate;
+        blog.save(function(err, blog) {
+          if (err) {
+            sendJSONresponse(res, 404, err);
+          } else {
+            sendJSONresponse(res, 200, blog);
+          }
+        });
+      }
+  );
+};
+
+/* DELETE /api/blogs/:blogid */
+module.exports.blogsDeleteOne = function(req, res) {
+  var blogid = req.params.blogid;
+  if (blogid) {
+    Blog
+      .findByIdAndRemove(blogid)
+      .exec(
+        function(err, blog) {
+          if (err) {
+            console.log(err);
+            sendJSONresponse(res, 404, err);
+            return;
+          }
+          console.log("blog id " + blogid + " deleted");
+          sendJSONresponse(res, 204, null);
+        }
+    );
+  } else {
+    sendJSONresponse(res, 404, {
+      "message": "No blogid"
+    });
+  }
+};
