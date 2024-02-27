de548d75987cca5814507365bd1bf9b138b2b579
kuhaku4 <edalmonr@millersville.edu>
2024-02-27 18:38:19 -0500

    happy times

--- blogger/app_server/controllers/blog.js
+++ blogger/app_server/controllers/blog.js
@@ -1,23 +1,222 @@
-exports.add = (req, res) => {
-    res.render('addBlog', { title: 'Add Blog' });
+var request = require('request');
+const { render } = require('../../app');
+var apiOptions = {
+  server : "http://localhost:80"  // Change as needed
+};
+var requestOptions = {
+  url : "http://52.91.47.28/api/blogs", method : "GET",
+  json : {},
+  qs: {}
+};
+
+/* GET blogs lists */      
+module.exports.list = function(req, res){
+  var requestOptions, path;
+  path = '/api/blogs';
+  requestOptions = { 
+      url : apiOptions.server + path,
+      method : "GET",
+      json : {},
+      qs : {} 
+      };
+  request(
+      requestOptions,
+      function(err, response, body) {
+          renderListPage(req, res, body);
+      }
+  );
+};
+
+/* Render the blog list page */
+var renderListPage = function(req, res, responseBody){
+  res.render('blogList', {
+      title: 'Blog List',
+      pageHeader: {
+          title: 'Blog List'
+      },
+      blogs: responseBody
+  });
+};
+
+
+/* Blog Show */
+module.exports.edit = function(req, res) {
+  var requestOptions, path;
+  path = "/api/blogs/" + req.params.id;
+  requestOptions = {
+      url : apiOptions.server + path,
+      method : "GET",
+      json : {}
+  }; 
+  request(
+      requestOptions,
+      function(err, response, body) {
+              renderEditPage(req, res, body);
+    }
+  );
+};
+
+/* Render the Blog edit page */
+var renderEditPage = function(req, res, responseBody){
+  res.render('blogEdit', {
+      title: 'Blog Edit',
+      pageHeader: {
+          title: 'Blog Edit'
+      },
+      blog: responseBody
+  });
+};
+
+
+/* Blog Edit Post */
+module.exports.editPost = function(req, res){
+  var requestOptions, path, postdata;
+  var id = req.params.id;
+  path = '/api/blogs/' + id;
+
+  postdata = {
+      blogtitle: req.body.blogtitle,
+      blogtext: req.body.blogtext
   };
 
-exports.list = (req, res) => {
-    res.render('blogList', {title: 'Blog List',
-      blogs: [{
-        blogtitle: 'test',
-        blogtext: 'test text',
-        createdDate: Date.now()
-      },
-    {
-      blogtitle: 'Nah I\'d Win',
-      blogtext: 'Didn\'t Win',
-      createdDate: Date.now()
-    },
-    {
-      blogtitle: 'My First Blog',
-      blogtext: 'This is a boring blog',
-      createdDate: Date.now()
-    }]
-})
+  requestOptions = {
+      url : apiOptions.server + path,
+      method : "PUT",
+      json : postdata
+  };
+
+  request(
+requestOptions,
+      function(err, response, body) {
+          if (response.statusCode === 201) {
+              res.redirect('/list');
+          } else {
+              _showError(req, res, response.statusCode);
+          }
+      }
+  );
 };
+
+/* Render the blog show page */
+// var renderShowPage = function(req, res, responseBody){
+//   res.render('blogList', {
+//       title: 'Blog Info',
+//       pageHeader: {
+//               title: 'Blog Info'
+//       },
+//       blog: responseBody
+//   });
+// };
+
+/* Blog Add */
+module.exports.add = function(req, res) {
+  res.render('addBlog', { title: 'Add Blog' });
+};    
+
+/* Blog Add Post */
+module.exports.addPost = function(req, res){
+  var requestOptions, path, postdata;
+  path = '/api/blogs/';
+
+  postdata = {
+      blogtitle: req.body.blogtitle,
+      blogtext: req.body.blogtext
+  }; 
+
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "POST",
+    json : postdata
+  };
+  
+  request(
+    requestOptions,
+    function(err, response, body) {
+       if (response.statusCode === 201) {
+            res.redirect('/list');
+       } else {
+            _showError(req, res, response.statusCode);
+       } 
+    }
+  ); 
+};
+
+/* Blog Delete */
+module.exports.del = function(req, res) {
+  var requestOptions, path;
+  path = "/api/blogs/" + req.params.id;
+  requestOptions = {
+      url : apiOptions.server + path,
+      method : "GET",
+      json : {}
+  };
+  request(
+requestOptions,
+      function(err, response, body) {
+          renderDeletePage(req, res, body);
+      }
+  );
+};
+
+/* Render the blook delete page */
+var renderDeletePage = function(req, res, responseBody){
+      res.render('deleteBlog', {
+      title: 'Blog Delete',
+      pageHeader: {
+              title: 'blog Delete'
+      },
+      blog: responseBody
+  });
+};
+
+/* Blog Delete Post */
+module.exports.deletePost = function(req, res){
+  var requestOptions, path, postdata;
+  var id = req.params.id;
+  path = '/api/blog/' + id;
+
+  requestOptions = {
+url : apiOptions.server + path,
+      method : "DELETE",
+      json : {}
+  };
+
+  request(
+      requestOptions,
+      function(err, response, body) {
+          if (response.statusCode === 204) {
+              res.redirect('/list');
+          } else {
+              _showError(req, res, response.statusCode);
+          }
+      }
+  );
+};
+
+
+
+
+
+// exports.add = (req, res) => {
+//     res.render('addBlog', { title: 'Add Blog' });
+//   };
+
+// exports.list = (req, res) => {
+//     res.render('blogList', {title: 'Blog List',
+//       blogs: [{
+//         blogtitle: 'test',
+//         blogtext: 'test text',
+//         createdDate: Date.now()
+//       },
+//     {
+//       blogtitle: 'Nah I\'d Win',
+//       blogtext: 'Didn\'t Win',
+//       createdDate: Date.now()
+//     },
+//     {
+//       blogtitle: 'My First Blog',
+//       blogtext: 'This is a boring blog',
+//       createdDate: Date.now()
+//     }]
+// })
+// };
--- blogger/app_server/routes/index.js
+++ blogger/app_server/routes/index.js
@@ -5,8 +5,26 @@ var crtlBlog = require('../controllers/blog');
 
 /* GET home page. */
 router.get('/', crtlHome.home);
-router.get('/list', crtlBlog.list);
-router.get('/add', crtlBlog.add);
-router.get('/')
+
+/* GET blogs lists */
+router.get('/', blogsCtrl.list);
+
+/* Blog Show */
+router.get('/blog/:id', blogsCtrl.edit);
+
+/* Blog Edit Post */
+router.put('/blog/:id', blogsCtrl.editPost);
+
+/* Blog Add */
+router.get('/blog/add', blogsCtrl.add);
+
+/* Blog Add Post */
+router.post('/blog/add', blogsCtrl.addPost);
+
+/* Blog Delete */
+router.get('/blog/:id/delete', blogsCtrl.del);
+
+/* Blog Delete Post */
+router.delete('/blog/:id/delete', blogsCtrl.deletePost);
 
 module.exports = router;
