670337bf924a1ae9fb03fb6c1bac3002b0a5efdf
kuhaku4 <edalmonr@millersville.edu>
2024-02-27 11:17:32 -0500

    list

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var ctrlBlogs = require('../controllers/blogs');
 
-router.get('/blog/list', ctrlBlogs.blogsList);
+//router.get('/blog/list', ctrlBlogs.blogsList);
 router.post('/blog/add', ctrlBlogs.blogsCreate);
 router.get('/blog/:blogid', ctrlBlogs.blogsReadOne);
 router.put('/blog/:blogid', ctrlBlogs.blogsUpdateOne);
