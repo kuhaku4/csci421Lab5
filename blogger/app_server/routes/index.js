f357730bac521c16fe44167ef1be40875b33f5c7
kuhaku4 <156698364+kuhaku4@users.noreply.github.com>
2024-02-27 13:04:32 -0500

    Update index.js

--- blogger/app_server/routes/index.js
+++ blogger/app_server/routes/index.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlHome = require('../controllers/homepage');
-var ctrlBlogs = require('../controllers/blog');
+var crtlHome = require('../controllers/homepage');
+var crtlBlog = require('../controllers/blog');
 
-router.get('/', ctrlHome.home);
-router.get('/blog', ctrlBlogs.blogList);
-router.get('/blog/add', ctrlBlogs.blogNew);
-router.post('/blog/add', ctrlBlogs.blogCreate)
-router.get('/blog/:blogid/edit', ctrlBlogs.blogEdit);
-//router.post('/blog/:blogid/save', ctrlBlogs.blogsUpdateOne);
-router.get('/blog/:blogid/delete', ctrlBlogs.blogEdit);
-router.delete('/blog/:blogid/delete', ctrlBlogs.blogDelete);
+/* GET home page. */
+router.get('/', crtlHome.home);
+router.get('/list', crtlBlog.list);
+router.get('/add', crtlBlog.add);
+router.get('/')
 
 module.exports = router;
