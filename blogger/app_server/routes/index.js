283cba78c7b281276c0670a95faaa6cea429c8b5
kuhaku4 <156698364+kuhaku4@users.noreply.github.com>
2024-02-26 22:14:41 -0500

    Update index.js

--- blogger/app_server/routes/index.js
+++ blogger/app_server/routes/index.js
@@ -1,19 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var crtlHome = require('../controllers/homepage');
-var crtlBlog = require('../controllers/blog');
+var ctrlHome = require('../controllers/home');
+var ctrlBlog = require('../controllers/blog');
 
-/* GET home page. */
-router.get('/', crtlHome.home);
-router.post('/complete/:id', crtlBlog.commitComplete);
-router.get('/list', crtlBlog.list);
-router.get('/add', crtlBlog.addBlog);
-router.post('/add', crtlBlog.saveBlog);
-router.get('/delete/:id',crtlBlog.deleteTask);
-router.post('/delete/:id',crtlBlog.confirmDelete);
-router.get('/edit/:id',crtlBlog.blogEdit);
-router.post('/edit/:id',crtlBlog.commitEdit);
+router.get('/', ctrlHome.home);
 
-router.get('/add')
-/* TEST7*/
+router.get('/blog', ctrlBlog.blogList);
+router.get('/blog/add', ctrlBlog.blogNew);
+router.post('/blog/add', ctrlBlog.blogAdd)
+router.get('/blog/:blogId/edit', ctrlBlog.blogEdit);
+router.post('/blog/:blogId/save', ctrlBlog.doBlogEdit);
+router.get('/blog/:blogId/delete', ctrlBlog.blogDelete);
+router.post('/blog/:blogId/delete', ctrlBlog.doBlogDelete);
+
 module.exports = router;
