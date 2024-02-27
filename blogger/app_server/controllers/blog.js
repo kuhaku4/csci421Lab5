aadd2fd968984cf94e81a3cfbfb90d0fc3c7d455
kuhaku4 <156698364+kuhaku4@users.noreply.github.com>
2024-02-26 19:23:06 -0500

    Update blog.js

--- blogger/app_server/controllers/blog.js
+++ blogger/app_server/controllers/blog.js
@@ -3,6 +3,75 @@ exports.add = (req, res) => {
   };
 
 var request = require('request');
+var apiOptions = {
+  server : "http://52.91.47.28:80"
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "http://52.91.47.28";
+}
+
+var _isNumeric = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var _formatDistance = function (distance) {
+  var numDistance, unit;
+  if (distance && _isNumeric(distance)) {
+    if (distance > 1) {
+      numDistance = parseFloat(distance).toFixed(1);
+      unit = 'km';
+    } else {
+      numDistance = parseInt(distance * 1000,10);
+      unit = 'm';
+    }
+    return numDistance + unit;
+  } else {
+    return "?";
+  }
+};
+
+
+
+var _showError = function (req, res, status) {
+  var title, content;
+  if (status === 404) {
+    title = "404, page not found";
+    content = "Oh dear. Looks like we can't find this page. Sorry.";
+  } else if (status === 500) {
+    title = "500, internal server error";
+    content = "How embarrassing. There's a problem with our server.";
+  } else {
+    title = status + ", something's gone wrong";
+    content = "Something, somewhere, has gone just a little bit wrong.";
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title : title,
+    content : content
+  });
+};
+
+var renderHomepage = function(req, res, responseBody){
+  var message;
+  if (!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No blogs";
+    }
+  }
+  res.render('blogList', {
+    title: 'List of Blogs',
+    pageHeader: {
+      title: 'Kuhaku',
+      strapline: 'Read Amazing Blogs'
+    },
+    sidebar: "TEST",
+    locations: responseBody,
+    message: message
+  });
+};
 
 module.exports.addBlog = function(req,res){
   res.render('blog-add-form',{
