ba067317b955bc93f4c16cd2981f2b8799740db5
kuhaku4 <edalmonr@millersville.edu>
2024-02-27 18:11:26 -0500

    blogger

--- blogger/app_api/controllers/blogs.js
+++ blogger/app_api/controllers/blogs.js
@@ -38,89 +38,85 @@ module.exports.blogsList = function(req, res) {
     });
     return blogs;
   };
-  
-  // Create a new blog
-  module.exports.blogsCreate = function(req, res) {
-    console.log('Creating New Blog');
-    Blogger.create({
-      blogtitle: req.body.blogtitle,
-      blogtext: req.body.blogtext
-    }, function(err, blog) {
-      if (err) {
-        sendJSONresponse(res, 400, err);
-      } else {
-        sendJSONresponse(res, 201, blog);
-      }
-    });
-  };
-  
+
   // Read a single blog
   module.exports.blogsReadOne = function(req, res) {
-    console
-    Blogger
-      .findById(req.params.blogid)
-      .exec(function(err, blog) {
-        if (err) {
-          sendJSONresponse(res, 404, err);
-        } else {
-          sendJSONresponse(res, 200, blog);
-        }
-      });
-  };
-  
-  // Update a single blog
-  module.exports.blogsUpdate = function(req, res) {
-    if (!req.params.blogid) {
-      sendJSONresponse(res, 404, {
-        "message": "blogid required"
-      });
-      return;
-    }
-    Blogger
-      .findById(req.params.blogid)
-      .exec(
-        function(err, blog) {
-          if (err) {
-            sendJSONresponse(res, 400, err);
-            return;
-          } else if (!blog) {
-            sendJSONresponse(res, 404, {
-              "message": "blogid not found"
-            });
-            return;
-          }
-          blog.blogtitle = req.body.blogtitle;
-          blog.blogtext = req.body.blogtext;
-  
-          blog.save(function(err, blog) {
-            if (err) {
-              sendJSONresponse(res, 404, err);
-            } else {
-              sendJSONresponse(res, 200, blog);
-            }
-          });
-        }
-      );
-  };
-  
-  // Delete a single blog
-  module.exports.blogsDelete = function(req, res) {
-    var blogid = req.params.blogid;
-    if (blogid) {
-      Blogger
-        .findByIdAndRemove(blogid)
-        .exec(
-          function(err, blog) {
-            if (err) {
+    console.log('Finding blog details', req.params);
+    if (req.params && req.params.blogid) {
+        Loc
+          .findById(req.params.blogid)
+          .exec(function(err, blog) {
+            if (!blog) {
+              sendJSONresponse(res, 404, {
+                "message": "blogid not found"
+              });
+              return;
+            } else if (err) {
+              console.log(err);
               sendJSONresponse(res, 404, err);
               return;
             }
-            sendJSONresponse(res, 204, null);
-          }
+            console.log(blog);
+            sendJSONresponse(res, 200, blog);
+          });
+      } else {
+        console.log('No blogid specified');
+        sendJSONresponse(res, 404, {
+          "message": "No blogid in request"
+        });
+      }
+    };
+  
+  // Create a new blog
+  module.exports.blogsCreate = function(req, res) {
+    console.log(req.body);
+    Blogger
+     .create({
+        blogtitle: req.body.blogtitle,
+        blogtext: req.body.blogtext,
+       }, function(err, blog) {
+         if (err) {
+            console.log(err);
+            sendJSONresponse(res, 400, err);
+         } else {
+            console.log(blog);
+            sendJSONresponse(res, 201, blog);
+         }
+       }
+     );
+  }; 
+  
+  // Update a single blog
+  module.exports.blogsUpdate = function(req, res) {
+    console.log("Updating a blog entry with id of " + req.params.blogid);
+    console.log(req.body);
+    Blogger
+  	  .findOneAndUpdate(
+	     { _id: req.params.blogid },
+ 	     { $set: {"blogtitle": req.body.blogtitle, "blogtext": req.body.blogtext}},
+	     function(err, response) {
+	         if (err) {
+	  	         sendJSONresponse(res, 400, err);
+	         } else {
+		        sendJSONresponse(res, 201, response);
+	        }
+	    }
+    );
+}; 
+  
+  // Delete a single blog
+  module.exports.blogsDelete = function(req, res) {
+    console.log("Deleting blog entry with id of " + req.params.blogid);
+    console.log(req.body);
+    Blogger
+        .findByIdAndRemove(req.params.blogid)
+        .exec (
+            function(err, response) {
+                if (err) {
+                            sendJSONresponse(res, 404, err);
+                } else {
+                            sendJSONresponse(res, 204, null);
+                }
+            }
         );
-    } else {
-      sendJSONresponse(res, 404, {
-        "message": "No blogid"
-      });
-    }
-  };
+};  
