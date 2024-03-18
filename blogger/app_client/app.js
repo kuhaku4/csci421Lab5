fe8b5355408b1b12564b681a0842025873bd9fe5
kuhaku4 <edalmonr@millersville.edu>
2024-03-18 19:31:16 -0400

    adinwaiudniawd

--- blogger/app_client/app.js
+++ blogger/app_client/app.js
@@ -12,18 +12,30 @@
         .otherwise({redirectTo: '/'});
       $routeProvider
         .when('/blogs', {
-
+            templateUrl: 'home/index.html',
+            controller: 'listCtrl',
+            controllerAs: 'vm'
         })
         .otherwise({redirectTo: '/'});
       $routeProvider
         .when('/blogs/add', {
-
+            templateUrl: 'home/index.html',
+            controller: 'addCtrl',
+            controllerAs: 'vm'
         })
         .otherwise({redirectTo: '/'});
+      $routeProvider
+        .when('/blog/:blogid/edit', {
+            templateUrl: 'home/index.html',
+            controller: 'editCtrl',
+            controllerAs: 'vm'
+        })
+        .otherwise({redirectTo: '/'});
+      $routeProvider
+        .when('/blog/:blogid/delete', {
+            templateUrl: 'home/index.html',
+            controller: 'delCtrl',
+            controllerAs: 'vm'
+        })
     }
-  
-    angular
-      .module('bloggerApp')
-      .config(['$routeProvider', config]);
-  
   })();
