a4202f07a9b4355dbc7a8eca3be786d2f716615e
kuhaku4 <edalmonr@millersville.edu>
2024-03-20 16:06:44 -0400

    phhhhhhphhh

--- blogger/app_client/home/bloggerApp.js
+++ blogger/app_client/home/bloggerApp.js
@@ -1,83 +1,34 @@
 (function () {
 
-  angular
+    angular
+      .module('bloggerApp')
+      .controller('homeCtrl', homeCtrl);
+  
+    homeCtrl.$inject = ['$scope'];
+    function homeCtrl ($scope) {
+      var vm = this;
+      vm.pageHeader = {
+        title: 'blogger',
+        strapline: 'Blogs'
+      };
+      vm.sidebar = {
+        content: "Blogger"
+      };
+  
+      vm.showError = function (error) {
+        $scope.$apply(function() {
+          vm.message = error.message;
+        });
+      };
+  
+    }
+    angular
     .module('bloggerApp')
-    .controller('homeCtrl', homeCtrl);
+    .controller('blogCtrl', addCtrl);
 
-  homeCtrl.$inject = ['$scope', '$location'];
-  function homeCtrl ($scope, $location) {
-    var vm = this;
-    vm.pageHeader = {
-      title: 'blogger',
-      strapline: 'Blogs'
-    };
-    vm.sidebar = {
-      content: "Blogger"
-    };
-
-    vm.viewBlog = function (blogId) {
-      $location.path('/blogs/' + blogId);
-    };
-
-    vm.goToAddBlog = function () {
-      $location.path('/blogs/add');
-    };
+  blogCtrl.$inject = ['$scope'];
+  function blogCtrl($scope){
+    
   }
-  angular
-  .module('bloggerApp')
-  .controller('blogCtrl', blogCtrl);
-
-blogCtrl.$inject = ['$scope', '$location', 'blogService'];
-function blogCtrl ($scope, $location, blogService) {
-  var vm = this;
-  vm.blogs = [];
-  vm.blog = {};
-  vm.editing = false;
-  vm.pageHeader = {
-    title: 'Blog',
-    strapline: 'Blog List'
-  };
-  vm.sidebar = {
-    content: 'Blog List'
-  };
-
-  vm.addBlog = function () {
-    blogService.addBlog(vm.blog).then(function (blog) {
-      vm.blogs.unshift(blog);
-      vm.blog = {};
-    });
-  };
-
-  vm.editBlog = function (blog) {
-    vm.blog = blog;
-    vm.editing = true;
-  };
-
-  vm.updateBlog = function (blog) {
-    blogService.updateBlog(blog._id, blog).then(function (updatedBlog) {
-      var index = vm.blogs.findIndex(function (item) {
-        return item._id === updatedBlog._id;
-      });
-      vm.blogs[index] = updatedBlog;
-      vm.blog = {};
-      vm.editing = false;
-    });
-  };
-
-  vm.deleteBlog = function (blogId) {
-    blogService.deleteBlog(blogId).then(function () {
-      vm.blogs = vm.blogs.filter(function (blog) {
-        return blog._id !== blogId;
-      });
-    });
-  };
-
-  vm.loadBlogs = function () {
-    blogService.getBlogs().then(function (blogs) {
-      vm.blogs = blogs;
-    });
-  };
-
-  vm.loadBlogs();
-}
-})();
+  
+  })();
