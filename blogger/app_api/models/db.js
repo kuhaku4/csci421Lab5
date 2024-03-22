0ee703469240c0b3e427aa8f4ed98cd0d7e41b6f
kuhaku4 <edalmonr@millersville.edu>
2024-03-22 23:18:02 +0000

    a

--- blogger/app_api/models/db.js
+++ blogger/app_api/models/db.js
@@ -1,48 +1,47 @@
-const mongoose = require('mongoose');
+var mongoose = require( 'mongoose' );
+require('./blog');
 
-//main().catch(err => console.log(err));
+var gracefulShutdown;
+var dbURI = 'mongodb://blogs:blogger@localhost:27017/blogs';
 
-var dbURI = 'mongodb://blogs:blogs@localhost/Blog'
-
-//async function main() {
 mongoose.connect(dbURI);
-//}
-
 
 // Monitor and report when database is connected                      
 mongoose.connection.on('connected', function () {
-    console.log('Mongoose connected to ' + dbURI);
-  });
-  
-  // Monitor and report error connecting to database
-  mongoose.connection.on('error',function (err) {
-    console.log('Mongoose connection error: ' + err);
-  });
-  
-  // Monitor and report when database is disconnected
-  mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose disconnected');
-  });    
+  console.log('Mongoose connected to ' + dbURI);
+});
 
+// Monitor and report error connecting to database
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
 
-// Closes (disconnects) from Mongoose DB upon shutdown                       
+// Monitor and report when database is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+}); 
+// Closes (disconnects) from Mongoose DB upon shutdown    
 gracefulShutdown = function (msg, callback) {
-    mongoose.connection.close(function () {
-      console.log('Mongoose disconnected through ' + msg);
-      callback();
-    });
-  };
-  
-  // For nodemon restarts
-  process.once('SIGUSR2', function () {
-    gracefulShutdown('nodemon restart', function () {
-      process.kill(process.pid, 'SIGUSR2');
-  }); });
-  
-  // For app termination
-  process.on('SIGINT', function() {
-    gracefulShutdown('app termination', function () {
-      process.exit(0);
-  }); });
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
 
-require('./blogs');
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+}); });
+
+// For app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+}); });
+
+// For Heroku app termination
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function () {
+    process.exit(0);
+}); });
--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlBlogs = require('../controllers/blogs');
+var ctrlBlog = require('../controllers/blog');
 
-router.get('/blogs', ctrlBlogs.blogsList);
-
-router.post('/blogs/add', ctrlBlogs.blogsCreate);
-
-router.get('/blogs/:blogid', ctrlBlogs.blogsReadOne);
-
-router.put('/blogs/:blogid', ctrlBlogs.blogsUpdate);
-
-router.delete('/blogs/:blogid', ctrlBlogs.blogsDelete);
+// Blogs
+router.get('/blogs', ctrlBlog.blogReadList);
+router.get('/blogs/:blogid', ctrlBlog.blogReadOne);
+router.post('/blogs', ctrlBlog.blogCreateOne);
+router.put('/blogs/:blogid', ctrlBlog.blogUpdateOne);
+router.delete('/blogs/:blogid', ctrlBlog.blogDeleteOne);
 
 module.exports = router;
