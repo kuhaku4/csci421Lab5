b10356a0a2d862c77563cd847317e9b6390d1d76
kuhaku4 <edalmonr@millersville.edu>
2024-03-22 19:25:46 -0400

    awdsdawdasdawd

--- blogger/app_api/models/blog.js
+++ blogger/app_api/models/blog.js
@@ -1,18 +1,10 @@
 var mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    createdOn: {
-      type: Date,
-      default: Date.now,
-    }
+var blogSchema = new mongoose.Schema({ 
+    blogtitle: {type: String, required: true},
+    blogtext: {type: String, required: true},
+    createdDate: {type: Date, "default": Date.now}
 });
+
 
 mongoose.model('Blog', blogSchema);
--- blogger/app_api/models/db.js
+++ blogger/app_api/models/db.js
@@ -1,47 +1,48 @@
-var mongoose = require( 'mongoose' );
-require('./blog');
+const mongoose = require('mongoose');
 
-var gracefulShutdown;
-var dbURI = 'mongodb://blogs:blogger@localhost:27017/blogs';
+//main().catch(err => console.log(err));
 
+var dbURI = 'mongodb://blogs:blogs@localhost/Blog'
+
+//async function main() {
 mongoose.connect(dbURI);
+//}
+
 
 // Monitor and report when database is connected                      
 mongoose.connection.on('connected', function () {
-  console.log('Mongoose connected to ' + dbURI);
-});
-
-// Monitor and report error connecting to database
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose connection error: ' + err);
-});
-
-// Monitor and report when database is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose disconnected');
-}); 
-// Closes (disconnects) from Mongoose DB upon shutdown    
-gracefulShutdown = function (msg, callback) {
-  mongoose.connection.close(function () {
-    console.log('Mongoose disconnected through ' + msg);
-    callback();
+    console.log('Mongoose connected to ' + dbURI);
   });
-};
+  
+  // Monitor and report error connecting to database
+  mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+  });
+  
+  // Monitor and report when database is disconnected
+  mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+  });    
 
-// For nodemon restarts
-process.once('SIGUSR2', function () {
-  gracefulShutdown('nodemon restart', function () {
-    process.kill(process.pid, 'SIGUSR2');
-}); });
 
-// For app termination
-process.on('SIGINT', function() {
-  gracefulShutdown('app termination', function () {
-    process.exit(0);
-}); });
+// Closes (disconnects) from Mongoose DB upon shutdown                       
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+      console.log('Mongoose disconnected through ' + msg);
+      callback();
+    });
+  };
+  
+  // For nodemon restarts
+  process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+      process.kill(process.pid, 'SIGUSR2');
+  }); });
+  
+  // For app termination
+  process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function () {
+      process.exit(0);
+  }); });
 
-// For Heroku app termination
-process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown', function () {
-    process.exit(0);
-}); });
+require('./blogs');
